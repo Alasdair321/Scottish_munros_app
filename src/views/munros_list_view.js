b3919642f03e8775199bce7f08058fc42d024319
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-11 15:38:50 +0100

    changes with dropdown

--- src/views/munros_list_view.js
+++ src/views/munros_list_view.js
@@ -1,30 +1,32 @@
 const PubSub = require('../helpers/pub_sub.js');
 const MunroView = require('./munro_view.js');
 
-const MunroListView = function (container) {
+const MunroListView = function(container) {
   this.container = container;
 }
 
-MunroListView.prototype.bindEvents = function () {
+MunroListView.prototype.bindEvents = function() {
   PubSub.subscribe('Munro:Ready', (event) => {
     this.munros = event.detail;
     this.render();
   })
-  PubSub.subscribe('Select:region-selected', (event)=>{
+  PubSub.subscribe('Select:region-selected', (event) => {
     const selectedRegion = event.detail;
     this.renderRegion(selectedRegion);
   })
 };
 
-MunroListView.prototype.render = function () {
-  this.munros.forEach( ( munro ) => {
+MunroListView.prototype.render = function() {
+  this.container.innerHTML = '';
+  this.munros.forEach((munro) => {
     const munroView = new MunroView(this.container, munro)
     munroView.render();
   });
 };
 
-MunroListView.prototype.renderRegion = function (region) {
-  this.munros.forEach( ( munro ) => {
+MunroListView.prototype.renderRegion = function(region) {
+  this.container.innerHTML = '';
+  this.munros.forEach((munro) => {
     if (region === munro.region) {
       const munroView = new MunroView(this.container, munro)
       munroView.render();
